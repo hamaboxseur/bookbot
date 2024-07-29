eba05e48aa500340d8fc82e2ae32d2ce92e8ac72
hamaboxseur <bouyahi.mohamed.1@gmail.com>
2024-07-29 23:27:38 +0200

    add dictionary content all caracter

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 def main():
     book_path = ".gitignore/books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    # print(text)
     num = count_word(text)
     print(f"the number word is : {num}")
+    tab = count_lettre(text)
+    print(f"the dictionary is : {tab}")
 
 
 def count_word(text):
     tab = text.split()
     return len(tab) 
+def count_lettre(text):
+    dic = {}
+    text = text.lower()
+    for i in text:
+        if i in dic:
+            dic[i]+=1
+        else:
+            dic[i]=1
 
+        
+    return dic
 def get_book_text(path):
     with open(path) as f:
         return f.read()
