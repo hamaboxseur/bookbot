a2f9bd4b635d3c547724ac0e8241255482c1ea03
hamaboxseur <bouyahi.mohamed.1@gmail.com>
2024-07-29 22:11:54 +0200

    count the numbres word in book

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
     book_path = ".gitignore/books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
+    num = count_word(text)
+    print(f"the number word is : {num}")
 
+
+def count_word(text):
+    tab = text.split()
+    return len(tab) 
 
 def get_book_text(path):
     with open(path) as f:
